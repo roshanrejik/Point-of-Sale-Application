c7ff9ea0dc6e82d569b3cdec056ae083af1ecd7c
roshanrejik <roshanrejik@gmail.com>
2022-04-28 18:53:53 +0530

    changes made to the project

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import  Axios  from "axios";
 const axios=Axios.create({
-    baseURL:'https://dct-billing-app.herokuapp.com/api'
+    baseURL:'https://dct-pos-app.herokuapp.com/api'
 })
 export default axios
